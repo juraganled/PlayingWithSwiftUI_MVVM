9aeb304978430ecfdb2a9d1ff42744b0b1f92496
Ricky Suprayudi <juraganled@gmail.com>
2024-05-14 12:48:00 +0700

    fix favorite button on top of navigation

---
+++ PlayingWithSwiftUI_MVVM/FruitCount.swift
@@ -0,0 +1,18 @@
+//
+//  FruitCount.swift
+//  PlayingWithSwiftUI_MVVM
+//
+//  Created by Ricky Suprayudi on 14/05/24.
+//
+
+import SwiftUI
+
+struct FruitCount: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitCount()
+}
